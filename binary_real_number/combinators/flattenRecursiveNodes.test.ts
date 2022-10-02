eb8628a2a72114fdc657f0d292f2114dc27a7cb3
FujiHaruka <fizzyjohn1427@gmail.com>
2022-10-02 14:23:11 +0900

    Add unit test

--- binary_real_number/combinators/flattenRecursiveNodes.test.ts
+++ binary_real_number/combinators/flattenRecursiveNodes.test.ts
@@ -1,1 +1,121 @@
-// TODO
+import { assertEquals, describe, it } from "../../deps-test.ts";
+import { LiteralTokenNode, NamedTokenNode, TokenType } from "../token.ts";
+import { flattenRecursiveNodes } from "./flattenRecursiveNodes.ts";
+
+describe(flattenRecursiveNodes.name, () => {
+  it("does not change a literal token node", () => {
+    const node = new LiteralTokenNode({
+      value: "#",
+      startAt: 0,
+      endAt: 0,
+    });
+    assertEquals(flattenRecursiveNodes(node), node);
+  });
+
+  it("does not change non-recursive node", () => {
+    const node = new NamedTokenNode({
+      type: new TokenType("a"),
+      startAt: 0,
+      endAt: 1,
+      children: [
+        new NamedTokenNode({
+          type: new TokenType("b"),
+          startAt: 0,
+          endAt: 1,
+          children: [
+            new NamedTokenNode({
+              type: new TokenType("c"),
+              startAt: 0,
+              endAt: 1,
+              children: [
+                new LiteralTokenNode({
+                  value: "#",
+                  startAt: 0,
+                  endAt: 0,
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    });
+
+    assertEquals(flattenRecursiveNodes(node), node);
+  });
+
+  it("flatten recursive node", () => {
+    const tokenType = new TokenType("a");
+    const node = new NamedTokenNode({
+      type: tokenType,
+      startAt: 0,
+      endAt: 0,
+      children: [
+        new LiteralTokenNode({
+          value: "0",
+          startAt: 0,
+          endAt: 0,
+        }),
+        new NamedTokenNode({
+          type: tokenType,
+          startAt: 0,
+          endAt: 0,
+          children: [
+            new LiteralTokenNode({
+              value: "1",
+              startAt: 0,
+              endAt: 0,
+            }),
+            new NamedTokenNode({
+              type: tokenType,
+              startAt: 0,
+              endAt: 0,
+              children: [
+                new LiteralTokenNode({
+                  value: "2",
+                  startAt: 0,
+                  endAt: 0,
+                }),
+              ],
+            }),
+          ],
+        }),
+        new LiteralTokenNode({
+          value: "3",
+          startAt: 0,
+          endAt: 0,
+        }),
+      ],
+    });
+
+    assertEquals(
+      flattenRecursiveNodes(node),
+      new NamedTokenNode({
+        type: tokenType,
+        startAt: 0,
+        endAt: 0,
+        children: [
+          new LiteralTokenNode({
+            value: "0",
+            startAt: 0,
+            endAt: 0,
+          }),
+          new LiteralTokenNode({
+            value: "1",
+            startAt: 0,
+            endAt: 0,
+          }),
+          new LiteralTokenNode({
+            value: "2",
+            startAt: 0,
+            endAt: 0,
+          }),
+          new LiteralTokenNode({
+            value: "3",
+            startAt: 0,
+            endAt: 0,
+          }),
+        ],
+      }),
+    );
+  });
+});
